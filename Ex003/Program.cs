adcf208611d068391362643fa7d3e7b80e81aa8f
Turist <morozov130863@yandex.ru>
2022-09-16 17:25:42 +0300

    Homework

---
+++ Ex003/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+============================================Задача1
+Console.Clear();
+Console.Write("Введите число А:  ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число В:  ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine("Max = " + num1);   
+}
+else
+{
+     Console.WriteLine("Max = " + num2);    
+}
+=============================================Задача2
+Console.Write("Введите число А:  ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число В:  ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число C:  ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int Max = num1;
+   if (num2 > Max)
+{
+     Max = num2;
+}
+   if (num3 > Max)
+{
+   Max = num3;
+}
+Console.WriteLine("Max = " + Max);
+
+==================================================Задача3  
+
+Console.Write("Введите число :  ");
+int num = Convert.ToInt32(Console.ReadLine());
+    if (num%2 == 0)
+{
+    Console.WriteLine("Да, четное");
+}
+    else
+{
+    Console.WriteLine("Нет, нечетное");  
+}
+====================================================Задача4
+
+Console.Write("Введите число :  ");
+int num = Convert.ToInt32(Console.ReadLine());
+int N = 1;
+while (N <= num)
+{
+  N = N + 1;
+
+    if (N%2==0)
+{
+    Console.WriteLine(N); 
+}
+}
+
